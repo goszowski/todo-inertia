27ebbcc64401dd20f93d02c94afa8f5bebf7a637
Ярослав Гошовський <goszowski@gmail.com>
2023-05-01 18:30:36 +0300

    fix horizon access

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -30,7 +30,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user) {
-            return;
+            return true;
             return in_array($user->email, [
                 //
             ]);
