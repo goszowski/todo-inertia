57fbba82ac4ada5daf8a86d0708711cb0df889cd
Ярослав Гошовський <goszowski@gmail.com>
2023-05-05 12:52:13 +0300

    csrf-token

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,6 +39,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
+            'csrf-token' => csrf_token(),
             'user' => Auth::user(),
             'error' => Session::get('error'),
             'success' => Session::get('success'),
